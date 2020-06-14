0fc8c3966bc67f47703f20b58e903cf8fa5724fa
khabubundivhu <khabubundivh@gmail.com>
2020-06-14 17:27:46 +0200

    :zap: Project Timeline view created

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -41,12 +41,7 @@
       </ul>
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link" href="/missions">Project Timeline</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="https://github.com/khabz" target="_blank">
-            Github Repo
-          </a>
+          <a class="nav-link" href="/timeline">Project Timeline</a>
         </li>
       </ul>
     </div>
